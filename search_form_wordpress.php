5b15cb06aa3210172dc406a7a2ce631918c51ec5
André Rodríguez Sabino <andresabinocalvo@gmail.com>
2021-03-03 13:29:11 +0000

    Update search_form_wordpress.php

--- search_form_wordpress.php
+++ search_form_wordpress.php
@@ -1,10 +1,10 @@
-<php?
-// Filter searchs only by products
+<?
+// Filtrar buscas somente por produtos
 add_filter( 'pre_get_posts', 'custom_search' );
 function custom_search( $query ) {
-    if ( $query->is_search ) {
     if ( $query->is_search and !is_admin() ) {
         $query->set( 'post_type', array( 'product' ) );
     }
     return $query;
+}
 ?>
